ac80fba64ecd5b29b98f41bc7f700e3988c87630
Elang Argawana <elangargawana.15@gmail.com>
2025-07-07 01:42:54 +0700

    first commit

---
+++ lib/datas/icons.dart
@@ -0,0 +1,8 @@
+import 'package:sightway_mobile/models/sightway_icon.dart';
+
+List<SightwayIcon> registerIcons = [
+  SightwayIcon(icon: 'user'),
+  SightwayIcon(icon: 'monitor'),
+  SightwayIcon(icon: 'email'),
+  SightwayIcon(icon: 'password'),
+];
---
+++ lib/models/sightway_icon.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class SightwayIcon {
+  final String icon;
+  final String? title;
+  final Color? color;
+
+  SightwayIcon({required this.icon, this.title, this.color});
+}
